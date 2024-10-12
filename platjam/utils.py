6a30f135e4a19de3685754a6c548d3f257f6852b
Nate Annau <nateannau@gmail.com>
2024-10-12 16:11:03 -0700

    Fix bug w floor vs int

--- platjam/utils.py
+++ platjam/utils.py
@@ -5,11 +5,8 @@ import sys
 from typing import Optional
 
 
-type Coordinate = tuple[int, int]
-
-
 class Screen:
-    def __init__(self, dims: Coordinate, title: str = '', alpha: bool = False):
+    def __init__(self, dims: pg.Vector2, title: str = '', alpha: bool = False):
         pg.init()
         pg.font.init()
         pg.mixer.init()
@@ -38,32 +35,33 @@ class Screen:
              text: str,
              color: pg.Color,
              font: pg.font.Font,
-             location: Optional[Coordinate] = None,
+             location: Optional[pg.Vector2] = None,
              center: bool = False) -> None:
         if location is None:
-            location = (self.WIDTH // 2, self.HEIGHT // 2)
+            location = pg.Vector2(self.WIDTH / 2, self.HEIGHT // 2)
 
         rendered_text = font.render(text, True, color)
         if center:
             text_size = font.size(text)
-            location = (location[0] + (text_size[0] // 2),
-                        location[1] + (text_size[1] // 2 ))
-        self._canvas.blit(rendered_text, location)
 
-    def circle(self, location: Coordinate, radius: int, color: pg.Color):
+            location.x += text_size[0] // 2
+            location.y += text_size[1] // 2
+        self._canvas.blit(rendered_text, Screen.floor_vector(location))
+
+    def circle(self, location: pg.Vector2, radius: int, color: pg.Color):
         if self.is_onscreen(location, radius):
-            gfxdraw.aacircle(self._canvas, location[0], location[1], radius, color)
-            gfxdraw.filled_circle(self._canvas, location[0], location[1], radius, color)
+            gfxdraw.aacircle(self._canvas, int(location.x), int(location.y), radius, color)
+            gfxdraw.filled_circle(self._canvas, int(location.x), int(location.y), radius, color)
 
-    def rect(self, location: Coordinate, dims: Coordinate, color: pg.Color):
-        gfxdraw.box(self._canvas, (location, dims), color)
+    def rect(self, location: pg.Vector2, dims: pg.Vector2, color: pg.Color):
+        gfxdraw.box(self._canvas, (Screen.floor_vector(location), dims), color)
 
-    def blit(self, image: pg.Surface, location: Coordinate):
-        self._canvas.blit(image, location)
+    def blit(self, image: pg.Surface, location: pg.Vector2):
+        self._canvas.blit(image, Screen.floor_vector(location))
 
-    def is_onscreen(self, location: Coordinate, radius: int = 0) -> bool:
-        in_width = location[0] - radius > 0 and location[0] + radius < self.WIDTH
-        in_height = location[1] - radius > 0 and location[1] + radius < self.HEIGHT
+    def is_onscreen(self, location: pg.Vector2, radius: int = 0) -> bool:
+        in_width = location.x - radius > 0 and location.x + radius < self.WIDTH
+        in_height = location.y - radius > 0 and location.y + radius < self.HEIGHT
         return in_width and in_height
 
     def fill(self, color: pg.Color) -> None:
@@ -79,9 +77,13 @@ class Screen:
         pg.display.flip()
         return True
 
+    @staticmethod
+    def floor_vector(vec: pg.Vector2) -> tuple[int, int]:
+        return (int(vec.x), int(vec.y))
+
 
 # Load functions
-def load(file: str, extra_path: str = '', scale: Optional[Coordinate] = None) -> pg.Surface:
+def load(file: str, extra_path: str = '', scale: Optional[pg.Vector2] = None) -> pg.Surface:
     path = os.path.join(f'./platjam/assets/image/{extra_path}', file)
     image = pg.image.load(path)
     if scale is None:
@@ -91,7 +93,7 @@ def load(file: str, extra_path: str = '', scale: Optional[Coordinate] = None) ->
 
 
 # add mirror image argument
-def load_folder(folder_path: str, scale: Optional[Coordinate] = None):
+def load_folder(folder_path: str, scale: Optional[pg.Vector2] = None):
     textures: list[pg.Surface] = []
     for name in sorted(os.listdir(f'./platjam/assets/image/{folder_path}')):
         if not name.startswith('.'):
